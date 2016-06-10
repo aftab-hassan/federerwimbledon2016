d6fb2686bf557f54b8e5c8437d02afd9c2c9a90e
aftab90 <aftabonquora@gmail.com>
2016-06-11 02:48:54 +0300

    added R scripts

---
+++ display.php
@@ -0,0 +1,11 @@
+<?php
+
+echo "\n45 matches found...\n\n";
+for($i = 0; $i < 3 ;$i++)
+{
+    echo "Pulling data for 45 matches between Roger Federer and Novak Djokovic ... \n\n";
+    sleep(2);
+}
+echo "Data pull complete ...\n\n";
+
+?>
